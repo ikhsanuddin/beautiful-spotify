77646db69f3efc7cc1433241ccc381408628d6a3
Ikhsanuddin Syamsuri <mail.ikhsanuddin@gmail.com>
2023-05-13 16:39:38 +0700

    Remove index html and add Route exeption

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,6 +25,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|assets).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|assets|_ipx|static).*)',
   ],
 };
